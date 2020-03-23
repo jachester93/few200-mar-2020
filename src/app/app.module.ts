888f13d71925c670ad063af760672b64f4d9555a
jachester93 <jachester93@gmail.com>
2020-03-23 14:55:17 -0400

    end of day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ShoppingListComponent } from './components/shopping/components/shopping-list/shopping-list.component';
+import { ShoppingEntryComponent } from './components/shopping/components/shopping-entry/shopping-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ShoppingComponent,
+    NavComponent,
+    ShoppingListComponent,
+    ShoppingEntryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shopping/components/shopping-entry/shopping-entry.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-entry',
+  templateUrl: './shopping-entry.component.html',
+  styleUrls: ['./shopping-entry.component.css']
+})
+export class ShoppingEntryComponent implements OnInit {
+
+  @Output() itemAdded: EventEmitter<string> = new EventEmitter<string>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addIt(stuff: HTMLInputElement) {
+    this.itemAdded.emit(stuff.value);
+    stuff.value = '';
+    stuff.focus();
+  }
+
+}
